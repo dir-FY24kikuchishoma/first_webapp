327545373b624ff6a226f4cc36e498d3f6e8de32
dir-FY24kikuchishoma <shoma.kikuchi.e893@dirbato.co.jp>
2024-04-15 10:39:47 +0900

    Lesson16_1回目

--- first_webapp/src/test/GetServlet.java
+++ first_webapp/src/test/GetServlet.java
@@ -28,7 +28,6 @@ public class GetServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	    request.setCharacterEncoding("UTF-8");
 
 	    String q = request.getParameter("q");
 
